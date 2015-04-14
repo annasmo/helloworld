fd7a486a3a74d0f35da6e390a2282e38ca9d3bb0
Anna <annasmo@student.agh.edu.pl>
2015-04-14 20:12:08 +0200

    cp1250

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("oh, hello, i'm the doctor");
-        System.out.println("szczÄ™Å›Ä‡");
+        System.out.println("szczêœæ");
+        System.out.println("trying cp1250");
     }
 
 }
