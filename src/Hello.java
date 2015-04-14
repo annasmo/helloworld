cb6b47340dbc22eb3f96c007cb6fc4b13a2b2e13
Anna <annasmo@student.agh.edu.pl>
2015-04-14 20:24:59 +0200

    nowa m

--- src/Hello.java
+++ src/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("oh, hello, i'm the doctor");
         System.out.println("szczęść");
-        System.out.println("trying cp1250 śćż");
+        System.out.println("trying cp1250 śćż"); //nowa
+
     }
 
 }
